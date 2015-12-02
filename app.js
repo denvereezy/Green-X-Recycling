951fffedc707329982d5476d207504264342bb13
denver <denver@projectcodex.co>
2015-12-02 13:32:41 +0200

    start of app front-end

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+    exphbs  = require('express-handlebars'),
+    compression = require('compression'),
+    app = express(),
+    myConnection = require('express-myconnection'),
+    mysql = require('mysql'),
+    bodyParser = require('body-parser');
+    // animals = require('./routes/animals');
+
+    // var dbOptions = {
+    //       host: 'localhost',
+    //       user: 'biza',
+    //       password: 'password',
+    //       port: 3306,
+    //       database: 'recycling'
+    // };
+
+app.use(compression());
+// app.use(myConnection(mysql, dbOptions, 'single'));
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.render("index");
+});
+// app.post('/lostdog/add', animals.addDogComment);
+// app.post('/lostcat/add', animals.addCatComment);
+// app.post('/founddog/add', animals.adddogFound)
+
+var port = process.env.PORT || 8000;
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
